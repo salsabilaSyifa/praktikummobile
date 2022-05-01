f732e6b0bed49348c795397b971bfa5a7470cd74
Salsabila Syifa <2010817320004@mhs.ulm.ac.id>
2022-05-01 19:52:33 +0800

    hapus

--- modul4/app/src/main/java/com/example/sportslist/model/Sports.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sportslist.model
-
-import androidx.annotation.StringRes
-import androidx.annotation.DrawableRes
-
-data class Sports (
-    @StringRes val nama: Int,
-    @DrawableRes val gambar : Int,
-)
