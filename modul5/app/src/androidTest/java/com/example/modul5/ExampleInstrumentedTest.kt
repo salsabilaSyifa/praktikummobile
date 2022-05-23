0b0d0865e914de6a428993fd2180b8a32e0cefba
Salsabila Syifa <2010817320004@mhs.ulm.ac.id>
2022-05-24 01:14:19 +0800

    ganti nama package

--- modul5/app/src/androidTest/java/com/example/modul5/ExampleInstrumentedTest.kt
+++ modul5/app/src/androidTest/java/com/example/modul5/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.tampil
+package com.example.modul5
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tampil", appContext.packageName)
+        assertEquals("com.example.modul5", appContext.packageName)
     }
 }
--- modul5/app/src/main/java/com/example/modul5/MainActivity.kt
+++ modul5/app/src/main/java/com/example/modul5/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.tampil
+package com.example.modul5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tampil.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
