19763605c40f65b65a1d488e11269867b942de32
Salsabila Syifa <2010817320004@mhs.ulm.ac.id>
2022-06-05 19:48:48 +0800

    up modul5

---
+++ modul5/app/src/main/java/com/example/modul5/ui/PoetryDetailFragment.kt
@@ -0,0 +1,43 @@
+package com.example.modul5.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.modul5.R
+import com.example.modul5.databinding.FragmentPoetryDetailBinding
+
+class PoetryDetailFragment : Fragment() {
+    private val viewModel: PoetryViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentPoetryDetailBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        (activity as AppCompatActivity).supportActionBar?.title = viewModel.poem.value?.title
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> findNavController().navigate(R.id.action_poetryDetailFragment_to_poetryListFragment)
+        }
+        return true
+    }
+}
---
+++ modul5/app/src/main/java/com/example/modul5/ui/PoetryListFragment.kt
@@ -0,0 +1,41 @@
+package com.example.modul5.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.modul5.R
+import com.example.modul5.databinding.FragmentPoetryListBinding
+import com.google.android.material.divider.MaterialDividerItemDecoration
+
+class PoetryListFragment : Fragment(){
+    private val viewModel: PoetryViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentPoetryListBinding.inflate(inflater)
+        viewModel.getPoetryList()
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        binding.recyclerView.adapter = PoetryListAdapter(PoetryListener { poetry ->
+            viewModel.onPoetryClicked(poetry)
+            findNavController()
+                .navigate(R.id.action_poetryListFragment_to_poetryDetailFragment)
+        })
+
+        (activity as AppCompatActivity).supportActionBar?.title = "List puisi"
+
+        binding.recyclerView.addItemDecoration(MaterialDividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL))
+
+
+        return binding.root
+    }
+}
